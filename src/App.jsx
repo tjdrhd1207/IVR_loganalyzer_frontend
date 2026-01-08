e14aaf9dbcf6645bfb8238e3d97dc299dfd2229e
jaemin <whlthd1207@naver.com>
2026-01-08 16:52:58 +0900

    base변경

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,13 @@ function App({ data }) {
     e.preventDefault();
     setLoading(true);
 
-    let base64string = "";
+    let base64String = "";
     if (image) {
       const reader = new FileReader();
-      base64string = await new Promise((res) => {
-        reader.onload = () => resolveConfig(reader.result);
+      base64String = await new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = (error) => reject(error);
         reader.readAsDataURL(image);
       });
     }
